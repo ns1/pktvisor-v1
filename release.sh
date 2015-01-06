18917a23a226743da192d1ed87d419aafe92d769
Shannon Weyrick <sweyrick@nsone.net>
2015-01-06 17:04:58 -0500

    release build steps

--- release.sh
+++ release.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
+make clean
+make PREFIX=/opt/dnstop-ng CONFIG_TOOLS="dnstop-ng bpfc" CCACHE=ccache ETCDIR=/opt/dnstop-ng/etc
 make PREFIX=/opt/dnstop-ng CONFIG_TOOLS="dnstop-ng bpfc" CCACHE=ccache ETCDIR=/opt/dnstop-ng/etc install
