b2ef71adbc146b37594808b07e8819c69cc3f65b
Shannon Weyrick <sweyrick@nsone.net>
2015-01-12 18:08:17 -0500

    rule typo

--- release.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-make clean
-make PREFIX=/opt/pktvisor CONFIG_TOOLS="pktvisor bpfc" CCACHE=ccache ETCDIR=/opt/pktvisor/etc
-make PREFIX=/opt/pktvisor CONFIG_TOOLS="pktvisor bpfc" CCACHE=ccache ETCDIR=/opt/pktvisor/etc install
